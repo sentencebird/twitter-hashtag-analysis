719bf5aa565af84293516eaef206bc899f0b9c91
sentencebird <hoge@example.com>
2022-08-26 02:42:14 +0900

    update: サービスごとの切り替え

--- src/app.py
+++ src/app.py
@@ -13,6 +13,8 @@ import collections
 import requests
 import datetime
 
+st.set_page_config(page_title="AI Art Twitter Hashtags", layout="wide")
+
 japanize_matplotlib.japanize()
 
 DB_URL = os.environ.get("DB_URL")
@@ -20,28 +22,47 @@ engine = create_engine(DB_URL)
 
 
 @st.cache
-def fetch_tweets():
-    res = engine.execute('SELECT * FROM tweets').fetchall()
+def fetch_tweets(service):
+    res = engine.execute(f'SELECT * FROM {service}_tweets').fetchall()
+
+    def to_media_urls(text):
+        if text == "{}":
+            return []
+        return text[1:-1].split(",")
     return [{"index": r[0], "tokens": r[1]
-            [1:-1].split(","), "text": r[2], "url": r[3]}for r in res]
+            [1:-1].split(","), "text": r[2], "url": r[3], "media_urls": to_media_urls(r[4])}for r in res]
 
 
 @st.cache
-def tweet_urls_by_index():
-    tweets = fetch_tweets()
+def tweet_urls_by_index(service):
+    tweets = fetch_tweets(service)
     return {t["index"]: t["url"] for t in tweets}
 
 
 @st.cache
-def fetch_tweet_indices():
-    res = engine.execute('SELECT * FROM indices').fetchall()
-    return [{"index": r[0], "word": r[1], "indices": r[2][1:-1].split(",")}for r in res]
+def media_urls_by_index(service):
+    tweets = fetch_tweets(service)
+    return {t["index"]: t["media_urls"] for t in tweets}
 
 
 @st.cache
-def tweet_indices_by_word():
-    indices = fetch_tweet_indices()
+def fetch_tweet_indices(service):
+    # res = engine.execute(f'SELECT * FROM {service}_indices').fetchall()
+    res = engine.execute(
+        f"SELECT * FROM {service}_indices ORDER BY Count DESC").fetchall()
+    return [{"index": r[0], "word": r[1], "indices": r[2][1:-1].split(",")} for r in res]
+
+
+@st.cache
+def tweet_indices_by_word(service):
+    indices = fetch_tweet_indices(service)
     return {i["word"]: i["indices"] for i in indices}
+
+
+@st.cache
+def stopwords():
+    res = engine.execute('SELECT * FROM stopwords').fetchall()
+    return [r[1] for r in res]
 
 
 def embed_tweet(url):
@@ -51,60 +72,48 @@ def embed_tweet(url):
     components.html(html, height=500)
 
 
-@st.cache
-def npt():
-    df = pd.DataFrame(fetch_tweets())
+def href_image(img_url, href):
+    st.markdown(f'''
+    <a href="{href}">
+        <img src="{img_url}" width="100%"/>
+    </a>
+    ''', unsafe_allow_html=True)
 
+
+@st.cache
+def bar_ngram(service):
+    df = pd.DataFrame(fetch_tweets(service))
     npt = nlplot.NLPlot(df, target_col='tokens', output_file_path="src/")
-
-    stopwords = npt.get_stopword(top_n=0, min_freq=0)
-    stopwords += ["midjourney", "Midjourney", "https", "…",
-                  ",", "-", "t", "co", "さん", "の", "　", "#", "/", ".", "://", "_", "(", ")", '"', "'", "¥n", "¥n¥n", "@"]
-
-    npt.default_stopwords = stopwords
-    return npt
-
-
-st.set_page_config(layout="wide")
-st.title("#midjourney Analysis")
-
-with st.spinner("Loading tweet data..."):
-    npt = npt()
-
-
-@st.cache
-def bar_ngram():
-    global npt
-
     return npt.bar_ngram(
-        title='上位50単語',
+        title="",
         xaxis_label='単語',
         yaxis_label='頻度',
         ngram=1,
         top_n=200,
         height=3000,
-        stopwords=npt.default_stopwords,
+        stopwords=stopwords(),
     )
 
 
 @st.cache
-def wordcloud():
-    global npt
+def wordcloud(service):
+    df = pd.DataFrame(fetch_tweets(service))
+    npt = nlplot.NLPlot(df, target_col='tokens', output_file_path="src/")
 
     npt.wordcloud(
         max_words=100,
         max_font_size=100,
         colormap='tab20_r',
-        stopwords=npt.default_stopwords,
+        stopwords=stopwords(),
         save=True
     )
     word_counts = {k: v for k, v in collections.Counter(
-        list(itertools.chain.from_iterable(npt.df[npt.target_col].tolist()))).items() if k not in npt.default_stopwords}
+        list(itertools.chain.from_iterable(npt.df[npt.target_col].tolist()))).items() if k not in stopwords()}
 
     font_path = "src/ipaexg.ttf"
     wordcloud = WordCloud(
         background_color='white',
-        stopwords=npt.default_stopwords,
+        stopwords=stopwords(),
         colormap='tab20_r',
         width=1000,
         height=700,
@@ -113,9 +122,11 @@ def wordcloud():
 
 
 @st.cache
-def co_network():
-    global npt
-    npt.build_graph(stopwords=npt.default_stopwords, min_edge_frequency=1)
+def co_network(service):
+    df = pd.DataFrame(fetch_tweets(service))
+    npt = nlplot.NLPlot(df, target_col='tokens', output_file_path="src/")
+
+    npt.build_graph(stopwords=stopwords(), min_edge_frequency=10)
     npt.co_network(title='network', width=1000, height=700, save=True)
 
     fname = datetime.datetime.now().strftime("%Y-%m-%d_network.html")
@@ -123,31 +134,46 @@ def co_network():
     return HtmlFile.read()
 
 
-# topwords
-st.header("上位単語")
-fig = bar_ngram()
-st.plotly_chart(fig, use_container_width=True)
+service = st.sidebar.selectbox(
+    "サービス", ["Midjourney", "Stable Diffusion"]).replace(" ", "").lower()
+opt = st.sidebar.selectbox("表示", ["画像一覧", "テキスト分析"])
 
-# Word Cloud
-st.header("ワードクラウド")
-wordcloud = wordcloud()
-fig, ax = plt.subplots()
-plt.imshow(wordcloud, interpolation='bilinear')
-st.pyplot(fig)
+st.markdown(
+    f"# [#{service}](https://twitter.com/search?q=%23midjourney&src=recent_search_click) Tweets", unsafe_allow_html=True)
 
-# CoOccurrence Network
-st.header("共起語ネットワーク")
-source_code = co_network()
-components.html(source_code, width=1000, height=1000)
+if opt == "画像一覧":
+    indices = fetch_tweet_indices(service)
+    word = st.selectbox("キーワード", [""] + [
+                        index["word"] for index in indices])
 
-# TODO: 単語索引（ツイート埋め込み）
-st.header("ツイート一覧")
-indices = fetch_tweet_indices()
-word = st.selectbox("キーワード", [index["word"] for index in indices])
+    if len(word) > 0:
+        cols = st.columns(3)
+        tweet_indices = tweet_indices_by_word(service)[word]
+        j = 0
+        for i in tweet_indices:
+            # embed_tweet(tweet_urls_by_index(service)[int(i)])
+            urls = media_urls_by_index(service)[int(i)]
+            for url in urls:
+                col = cols[j % 3]
+                with col:
+                    href_image(url, tweet_urls_by_index(service)[int(i)])
+                    j += 1
 
-if len(word) > 0:
-    tweet_indices = tweet_indices_by_word()[word]
-    for i in tweet_indices:
-        embed_tweet(tweet_urls_by_index()[int(i)])
+elif opt == "テキスト分析":
 
-# TODO: 画像一覧
+    # topwords
+    st.header("上位単語")
+    fig = bar_ngram(service)
+    st.plotly_chart(fig, use_container_width=True)
+
+    # Word Cloud
+    st.header("ワードクラウド")
+    wordcloud = wordcloud(service)
+    fig, ax = plt.subplots()
+    plt.imshow(wordcloud, interpolation='bilinear')
+    st.pyplot(fig)
+
+    # CoOccurrence Network
+    st.header("共起語ネットワーク")
+    source_code = co_network(service)
+    components.html(source_code, width=1000, height=1000)
--- src/cron.py
+++ src/cron.py
@@ -5,6 +5,15 @@ from sqlalchemy import create_engine
 import pandas as pd
 import nlplot
 import numpy as np
+import string
+from tqdm import tqdm
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-s', default="midjourney")
+args = parser.parse_args()
+service = args.s
 
 tagger = MeCab.Tagger(
     '/usr/lib/aarch64-linux-gnu/mecab/dic/mecab-ipadic-neologd')
@@ -22,14 +31,17 @@ api = tweepy.API(auth)
 
 engine = create_engine(DB_URL)
 
+default_stopwords = ["midjourney", "Midjourney", "midjourneyart", "midjourneyAi", "ミッドジャーニー", "stablediffusion",
+                     "AI", "画像", "絵", "beta", "aiart", "of", "the", "in",
+                     "https", "…", ",", "-",  "co", "さん", "の", "　", "#", "/", ".", "://", "_", "(", ")", '"', "'", "¥n", "¥n¥n", "@"] + [s for s in string.ascii_letters]
 
-def parse_tokens(text):
+
+def parse_tokens(text, stopwords=default_stopwords):
     node = tagger.parseToNode(text)
     d = {}
     while node:
         pos = node.feature.split(',')[0]
-        if pos == '名詞' and node.feature.split(",")[1] != "数" or \
-                pos == '動詞' or pos == '形容詞':
+        if pos == '名詞' and node.feature.split(",")[1] in ["一般", "固有名詞"] and node.surface not in stopwords:
             d.setdefault(pos, [])
             d[pos].append(node.surface)
         node = node.next
@@ -37,36 +49,39 @@ def parse_tokens(text):
 
 
 # Tweet
-# res = api.search_tweets(
-#     "#midjourney exclude:retweets exclude:replies", lang="ja", count=100)
-res = tweepy.Cursor(api.search_tweets, "#midjourney exclude:retweets exclude:replies",
-                    lang="ja", count=100).items(1000)
-tweet_tokens, texts, urls = [], [], []
-for r in res:
+res = tweepy.Cursor(api.search_tweets, f"#{service} exclude:retweets exclude:replies",
+                    lang="ja", count=100).items(5000)
+tweet_tokens, texts, urls, media_urls = [], [], [], []
+for r in tqdm(res):
     text = r.text
     texts.append(text)
     tweet_tokens.append(parse_tokens(text))
     urls.append(f"https://twitter.com/{r.user.screen_name}/status/{r.id}")
+    media_urls.append([m["media_url"] for m in r.extended_entities['media']] if hasattr(
+        r, "extended_entities") else [])
 tgt_pos = ["名詞"]
 
 tgt_tokens = [np.hstack([tokens[pos] for pos in tgt_pos if pos in tokens]).tolist(
-) for tokens in tweet_tokens]
-df = pd.DataFrame({"tokens": tgt_tokens, "texts": texts, "urls": urls})
+) if tokens != {} else [] for tokens in tweet_tokens]
+df = pd.DataFrame({"tokens": tgt_tokens, "texts": texts,
+                  "urls": urls, "media_urls": media_urls})
 
-df.to_sql("tweets", con=engine, if_exists="replace")
-
+df.to_sql(f"{service}_tweets", con=engine, if_exists="replace")
 
 # Word Index
 npt = nlplot.NLPlot(df, target_col='tokens')
 
-stopwords = npt.get_stopword(top_n=0, min_freq=0)
-stopwords += ["midjourney", "Midjourney", "https", "…",
-              ",", "-", "t", "co", "さん", "の", "　", "#", "/", ".", "://", "_", "(", ")", '"', "'", "¥n", "¥n¥n", "@"]
+stopwords = npt.get_stopword(top_n=20)
+stopwords += default_stopwords
+
+pd.DataFrame(stopwords, columns=["words"]).to_sql(
+    "stopwords", con=engine, if_exists="replace")
+
 ngram_df = npt.df.copy()
 ngram_df.loc[:, 'space'] = ngram_df[npt.target_col].apply(
     lambda x: ' '.join(x))
 
-topwords = nlplot.nlplot.freq_df(ngram_df["space"], stopwords=stopwords, n=200)[
+topwords = nlplot.nlplot.freq_df(ngram_df["space"], stopwords=stopwords, n=1000)[
     "word"].tolist()
 
 indices_by_topword = {}
@@ -76,6 +91,7 @@ for i, row in npt.df.iterrows():
         indices_by_topword.setdefault(word, [])
         indices_by_topword[word].append(i)
 
-indices_df = pd.DataFrame(indices_by_topword.items(),
-                          columns=["Word", "Indices"])
-indices_df.to_sql("indices", con=engine, if_exists="replace")
+d = [{"word": word, "tweet_indices": indices, "count": len(
+    indices)} for word, indices in indices_by_topword.items()]
+indices_df = pd.DataFrame(d)
+indices_df.to_sql(f"{service}_indices", con=engine, if_exists="replace")
